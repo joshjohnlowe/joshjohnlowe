912f07d045d40aa7adec9222da719ab7fe8e774c
Josh Lowe <joshlowe@Joshs-MacBook-Pro.local>
2020-04-27 14:45:47 +1200

    Case updates

---
+++ src/pages/about.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import '../cool/main.scss'
+
+import { Redirect } from 'react-router';
+
+import * as moment from 'moment';
+
+class About extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            redirectToHome: false,
+            currentMode: "Home",
+            date: moment().format('DD / MM / YY'),
+            time: moment().format('hh:mm:ss')
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1
+        );
+      }
+
+
+    _onMouseMove(e) {
+        const navvy_boi = document.querySelector(".nav_image")
+        navvy_boi.style.left = `${e.nativeEvent.offsetX}px`;
+        navvy_boi.style.top = `${e.nativeEvent.offsetY}px`;
+    }
+
+    tick() {
+        this.setState({
+            date: moment().format('DD / MM / YY'),
+            time: moment().format('hh:mm:ss')
+        });
+    }
+
+
+
+    _onMouseClick(e) {
+        this.setState({
+            redirectToHome: true
+        });
+    }
+
+    _emailMouseOver(e) {
+        this.setState({
+            currentMode: "Copy Email"
+        })
+    }
+
+    _digitsMouseOver(e) {
+        this.setState({
+            currentMode: "Copy No."
+        })
+    }
+
+    _onMousegGap(e) {
+        this.setState({
+            currentMode: "Home"
+        })
+    }
+
+
+    render() {
+        var { redirectToHome } = this.state;
+
+        if (redirectToHome) {
+            return <Redirect to='/' />;
+        }
+        return (
+            <div onMouseMove={this._onMouseMove.bind(this)}
+                onMouseUp={this._onMouseClick.bind(this)}>
+                <div className="nav_image">
+                    <img src={require("../media/back_arrow.svg")}/>
+                </div>
+                <div
+                    className="full_width"
+
+                >
+                    <div className="half_width" >
+
+                    </div>
+                </div>
+                <div className="left_pane">
+                    <div className="upper_header" >
+                        <h1> <span className="thin_text">(</span>{this.state.time}<span className="thin_text">)</span> </h1>
+                        <h2>
+                            Joshua Lowe is a Software / Data Engineer from Wellington, New Zealand.
+                            He has a degree in Computer Science from Victoria University of Wellington. 
+                        </h2>
+                        <h2>
+                            <span className="email_addy"
+                                onMouseOver={this._emailMouseOver.bind(this)}
+                                onMouseLeave={this._onMousegGap.bind(this)}
+                            >
+                                hello@joshjohnlowe.com
+                            </span> <br />
+                            <span className="digits"
+                                onMouseOver={this._digitsMouseOver.bind(this)}
+                                onMouseLeave={this._onMousegGap.bind(this)}
+                            >
+                                +(64) 21 073 7996
+                            </span> <br />
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About
